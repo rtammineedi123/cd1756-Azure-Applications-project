9d91f9274541a8fe16698cb0166f4b7337850c1d
rtammineedi123 <rajanibalusu@gmail.com>
2025-01-29 18:57:20 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,24 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+if not os.path.exists('logs'):
+  os.mkdir('logs')
+
+path = os.path.join('logs','flaskapp.log')
+
+logging.basicConfig(
+    filename = path,
+    level=logging.INFO,
+    format='%(pathname)s:%(lineno)d - %(message)s; %(asctime)s [%(levelname)s]'
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler_format = logging.Formatter('%(message)s - %(asctime)s')
+handler.setFormatter(handler_format)
+
+app.logger.addHandler(handler)
+app.logger.info('Starting WebProject')
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
